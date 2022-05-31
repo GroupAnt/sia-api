1f561d0115d2e74d7dae3b267cfafa3425659124
carloserocha <carloserocha27@gmail.com>
2022-05-31 19:50:35 -0300

    change to url connect database

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,11 +10,7 @@ import { NotificationsModule } from './notifications/notifications.module';
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
+      url: process.env.DATABASE_URL,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: process.env.DB_SYNCHRONIZE === 'true',
       logging: process.env.DB_LOGGING === 'true',
